28a4c67f16d618ec418e887b3af078450fd4bd8d
testsekita <sekitar2002@gmail.com>
2021-08-13 20:47:24 -0600

    configuracion

--- Menu/settings.py
+++ Menu/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'e15pp5a3!l2%bf7_ejx0l^5v!+4w1s89-5qwoxc7v3s6_!=t@&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
